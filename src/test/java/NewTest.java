c132e0058ee30ffe0aad3f6b47f96bce931b1947
fantasy <820438873@qq.com>
2021-11-30 19:09:14 +0800

    4

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,38 +1,35 @@
 import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
 
-@Ignore
 public class NewTest {
     Calculate calcuate;
     @Test
     public void testAdd() {
         calcuate = new Calculate();
-        int result = calcuate.add(2, 3);
-        Assert.assertEquals(result,5);
-        /*
-         * "加法有问题"：期望值和实际值不一致时，显示的信息
-         * 5 ：期望值
-         * result ：实际值
-         */
-
+        int result = calcuate.add(3, 7);
+        Assert.assertEquals(result,10);
     }
     @Test
     public void testSubtract() {
         calcuate = new Calculate();
-        int result = calcuate.subtract(12, 2);
-        Assert.assertEquals(result,10); //故意设置减法期望值为10000
+        int result = calcuate.subtract(10, 2);
+        Assert.assertEquals(result,8);
     }
     @Test
     public void testMultiply() {
         calcuate = new Calculate();
-        int result = calcuate.multiply(2, 3);
-        Assert.assertEquals(result, 6);
+        int result = calcuate.multiply(4, 5);
+        Assert.assertEquals(result, 20);
     }
     @Test
     public void testDivide() {
         calcuate = new Calculate();
-        int result = calcuate.divide(6, 3);
+        int result = calcuate.divide(18, 9);
         Assert.assertEquals(result, 2);
     }
 }
